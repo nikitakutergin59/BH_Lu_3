568d411e1df05a6491a5bc7292d41b8d82d94784
♰Burning_Head♰ <nikitakutergin59@gmail.com>
2025-05-08 22:50:00 +0300

    Delete autorun.bat
    заменяем нф более универсальный

--- autorun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start cmd /k "cd C:\Users\user\BH_Lu_3\BH_Lu_3\cmd\runBH && set PATH=%PATH%;C:\Program Files\Go\bin && go run BHmain.go"
-start cmd /k "cd C:\Users\user\BH_Lu_3\BH_Lu_3\cmd\runOrchestrator && set PATH=%PATH%;C:\Program Files\Go\bin && go run Omain.go"
-start cmd /k "cd C:\Users\user\BH_Lu_3\BH_Lu_3\cmd\runDemon && set PATH=%PATH%;C:\Program Files\Go\bin && go run Dmain.go"
-pause
