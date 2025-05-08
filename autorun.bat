01fbeee2211aeeb59af9ca124534d4234ba8cfd0
♰Burning_Head♰ <nikitakutergin59@gmail.com>
2025-05-08 23:07:42 +0300

    Update autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,5 +1,5 @@
 @echo off
-start cmd /k "go run ./BH_Lu_3/cmd/runBH/BHmain.go"
-start cmd /k "go run ./BH_Lu_3/cmd/runOrchestrator/Omain.go"
-start cmd /k "go run ./BH_Lu_3/cmd/runDemon/Dmain.go"
+start cmd /k "go run ./BH_Lu_3/BH_Lu_3/cmd/runBH/BHmain.go"
+start cmd /k "go run ./BH_Lu_3/BH_Lu_3/cmd/runOrchestrator/Omain.go"
+start cmd /k "go run ./BH_Lu_3/BH_Lu_3/cmd/runDemon/Dmain.go"
 pause
